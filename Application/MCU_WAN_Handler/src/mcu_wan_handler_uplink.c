f79c41d8717b1cb15920965ca34fe569d00715e6
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2026-05-06 10:36:59 +0700

    Update mcu_wan_handler_uplink.c

--- Application/MCU_WAN_Handler/src/mcu_wan_handler_uplink.c
+++ Application/MCU_WAN_Handler/src/mcu_wan_handler_uplink.c
@@ -749,7 +749,7 @@ esp_err_t mcu_wan_handler_start(void) {
                                   .gpio_io0 = 11,
                                   .gpio_io1 = 13,
                                   .gpio_data_ready_input = 45,
-                                  .clock_speed_hz = 10000000,
+                                  .clock_speed_hz = 60000000,
                                   .mode = 0,
                                   .host_id = SPI2_HOST,
                                   .dma_channel = SPI_DMA_CH_AUTO,
